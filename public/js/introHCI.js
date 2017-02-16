de52b77c6d18b10c4993bf0035e1094a1f8befd2
Shuai Huang <shh176@ucsd.edu>
2017-02-16 13:05:52 -0800

    lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,22 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	$.get('/project/'+idNumber, addProject);
+
+}
+
+function addProject(result){
+//	var projectHTML =' result["id"]'+ 'result["title"]' + 'result["date"]' + 'result["summary"]'+ '<img src ="'result["image"]'">';
+
+  var projectHTML ='<img src="' + result['image'] + '">' + 
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p>'+ '<p>'+ result['summary']+ '</p>'; 
+
+	$("#project"+result["id"]+ " .details").html(projectHTML);
+	
+	console.log(result["id"]); 
+
 }
 
 /*
@@ -34,5 +50,20 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
-	console.log("User clicked on color button");
+	$.get("/palette", addColor);
 }
+
+function addColor(result){
+	var v = result["colors"];
+	var colors = v["hex"];
+  	$('body').css('background-color', colors[0]);
+  	$('.thumbnail').css('background-color', colors[1]);
+  	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+  	$('p').css('color', colors[3]);
+  	$('.project img').css('opacity', .75);
+
+
+}
+
+
+
